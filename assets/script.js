7092f269ca554c2b8c35d01b4b498ac731e603af
CodeDroid999 <johnmwega2002@gmail.com>
2023-01-24 11:13:41 +0300

    add jquery and automatic js

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,21 @@
-var slideIndex= 1;
-showSlides(slideIndex)
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function plusSlides(n){
-    showSlides(slideIndex +=n);
-}   
-
-function currentSlide(n){
-    showSlides(slideIndex = n);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-function showSlides(n){
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
     var i;
-    var slides =document.getElementsByClassName("myslide");
-    var dots =document.getElementsByClassName("dot");
-    if (n>slides.length) {slideIndex= 1}
-    if (n<1) {slideIndex=slides.length}
-    for(i=0;i>slides.length;i++){
-        slide[1].style.display="none";
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    for(i=0;i>dots.length;i++){
-        dots[1].className=dots[1].className.replace("active","");
-       }
-       slides[slideIndex-1].style.display="block";
-       dots[slideIndex-1].className += "active";
-} 
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
